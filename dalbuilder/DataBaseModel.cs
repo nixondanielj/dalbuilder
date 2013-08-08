9358a0c7534e369518dad95137565a9f3eb0299e
Dan Nixon <dannix7700@gmail.com>
2013-08-08 14:51:02 -0400

    Initial Commit

---
+++ dalbuilder/DataBaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DALBuilder
+{
+    class DataBaseModel
+    {
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+        public List<TableModel> Tables { get; set; }
+        public List<AssociationModel> Associations { get; set; }
+    }
+}
---
+++ dalbuilder/TableModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DALBuilder
+{
+    class TableModel
+    {
+        public string Name { get; set; }
+        public string ModelName { get; set; }
+        public ColumnModel PrimaryKey { get; set; }
+        public List<ColumnModel> ForeignKeys { get; set; }
+        public List<ColumnModel> Columns { get; set; }
+        public int object_id { get; set; }
+        public bool JoinOnly { get; set; }
+
+        public TableModel()
+        {
+            ForeignKeys = new List<ColumnModel>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
