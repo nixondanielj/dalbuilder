3c87d15cb2af1ec423f90f4d8ce0ffb4f19a7696
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-01-29 15:58:24 -0500

    added query service, started populating iqueryservice

---
+++ DALBuilder/DBModel/IQueryService.cs
@@ -0,0 +1,18 @@
+﻿using DALBuilder.DBModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALBuilder.DBModel
+{
+    interface IQueryService
+    {
+        List<Table> GetRawTables();
+
+        void PopulateColumns(Table model);
+
+        Column GetPrimaryColumn(Table table);
+    }
+}
